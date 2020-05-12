ed56f669c009a5eecaedc7612b08d9198b5b8a73
Josh Enders <josh.enders@gmail.com>
2020-05-12 01:14:15 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# userscript-landsbankinn-netbanki
+
+This userscript removes the hide/show elements from Netbanki login fields to
+allow Safari autofill.
+
+## License
+
+This work is licensed under CC BY-NC version 4.0 https://creativecommons.org/licenses/by-nc/4.0/
+© 2020, Josh Enders. Some Rights Reserved.
---
+++ landsbankinn-netbanki.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Landsbankinn Netbanki Safari Autofill Fixer
+// @description  Removes the hide/show elements from Netbanki login fields to allow Safari autofill
+// @namespace    https://joshenders.com
+// @version      v1.0.0
+// @author       Josh Enders <josh.enders@gmail.com>
+// @updateURL    https://raw.githubusercontent.com/joshenders/userscript-landsbankinn-netbanki/stable/landsbankinn-netbanki.user.js
+// @downloadURL  https://raw.githubusercontent.com/joshenders/userscript-landsbankinn-netbanki/stable/landsbankinn-netbanki.user.js
+// @grant        none
+// @match        *://netbanki.landsbankinn.is/*
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementsByClassName('eye eye-open js-toggle-username-hide')[0].remove();
+    document.getElementsByClassName('eye eye-closed js-toggle-password-show')[0].remove();
+})();
