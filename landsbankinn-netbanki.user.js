5084df04647d3b25a88affecfcd234e0225c8ba0
Josh Enders <josh.enders@gmail.com>
2021-02-11 21:05:11 +0000

    updates tampermonkey heading

--- landsbankinn-netbanki.user.js
+++ landsbankinn-netbanki.user.js
@@ -2,10 +2,10 @@
 // @name         Landsbankinn Netbanki Safari Autofill Fixer
 // @description  Removes the hide/show elements from Netbanki login fields to allow Safari autofill
 // @namespace    https://joshenders.com
-// @version      v1.0.0
+// @version      v1.0.1
 // @author       Josh Enders <josh.enders@gmail.com>
-// @updateURL    https://raw.githubusercontent.com/joshenders/userscript-landsbankinn-netbanki/stable/landsbankinn-netbanki.user.js
-// @downloadURL  https://raw.githubusercontent.com/joshenders/userscript-landsbankinn-netbanki/stable/landsbankinn-netbanki.user.js
+// @updateURL    https://raw.githubusercontent.com/joshenders/userscript-landsbankinn-netbanki/master/landsbankinn-netbanki.user.js
+// @downloadURL  https://raw.githubusercontent.com/joshenders/userscript-landsbankinn-netbanki/master/landsbankinn-netbanki.user.js
 // @grant        none
 // @match        *://netbanki.landsbankinn.is/*
 // @run-at       document-end
